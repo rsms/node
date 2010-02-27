a4f5264e2dcbc806e117363aaac1261b2bd6ceac
Rasmus Andersson <rasmus@notion.se>
2010-02-27 18:12:49 +0100

    ry-master version of node_file.cc

--- src/node_file.cc
+++ src/node_file.cc
@@ -94,13 +94,6 @@ static int After(eio_req *req) {
         break;
       }
 
-      case EIO_READLINK:
-      {
-        argc = 2;
-        argv[1] = String::New(static_cast<char*>(req->ptr2), req->result);
-        break;
-      }
-
       case EIO_READ:
       {
         argc = 3;
